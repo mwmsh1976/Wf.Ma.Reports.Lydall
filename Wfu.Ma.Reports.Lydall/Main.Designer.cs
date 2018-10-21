fa59a5b31deef99d5f2f593807abdf60ddfd7f1b
Michael <mwmsh1976@gmail.com>
2018-10-21 01:34:53 -0400

    Initial commit

---
+++ Wfu.Ma.Reports.Lydall/Main.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Wfu.Ma.Reports.Lydall
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.BatchReportPath = new System.Windows.Forms.TextBox();
+			this.Browse = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.datePicker = new System.Windows.Forms.DateTimePicker();
+			this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox1.Controls.Add(this.datePicker);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.Browse);
+			this.groupBox1.Controls.Add(this.BatchReportPath);
+			this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(446, 73);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Choose batch file criteria";
+			// 
+			// BatchReportPath
+			// 
+			this.BatchReportPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.BatchReportPath.Location = new System.Drawing.Point(80, 19);
+			this.BatchReportPath.Name = "BatchReportPath";
+			this.BatchReportPath.Size = new System.Drawing.Size(324, 20);
+			this.BatchReportPath.TabIndex = 0;
+			// 
+			// Browse
+			// 
+			this.Browse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.Browse.Location = new System.Drawing.Point(403, 17);
+			this.Browse.Name = "Browse";
+			this.Browse.Size = new System.Drawing.Size(28, 23);
+			this.Browse.TabIndex = 1;
+			this.Browse.Text = "...";
+			this.Browse.UseVisualStyleBackColor = true;
+			this.Browse.Click += new System.EventHandler(this.Browse_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(19, 22);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(51, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Location:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(19, 46);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(33, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Date:";
+			// 
+			// datePicker
+			// 
+			this.datePicker.Enabled = false;
+			this.datePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+			this.datePicker.Location = new System.Drawing.Point(80, 45);
+			this.datePicker.Name = "datePicker";
+			this.datePicker.Size = new System.Drawing.Size(105, 20);
+			this.datePicker.TabIndex = 4;
+			// 
+			// openFileDialog
+			// 
+			this.openFileDialog.FileName = "openFileDialog1";
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(470, 101);
+			this.Controls.Add(this.groupBox1);
+			this.MaximizeBox = false;
+			this.Name = "Main";
+			this.Text = "Lydall - Daily Summary Report";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.DateTimePicker datePicker;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button Browse;
+		private System.Windows.Forms.TextBox BatchReportPath;
+		private System.Windows.Forms.OpenFileDialog openFileDialog;
+	}
+}
+
---
+++ Wfu.Ma.Reports.Lydall/Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wfu.Ma.Reports.Lydall
+{
+	public partial class Main : Form
+	{
+		public Main()
+		{
+			InitializeComponent();
+		}
+
+		private void Browse_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				 this.BatchReportPath.Text = openFileDialog.FileName;
+				if (BatchReportPath.Text.Trim() != "") { datePicker.Enabled = true; }
+			}
+		}
+	}
+}
